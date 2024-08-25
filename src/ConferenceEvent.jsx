904a1d7e6d16f243ac8053af76bbd400593d7f4b
KruBro <techkru697@gmail.com>
2024-08-25 21:38:15 +0530

    progessing

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -3,10 +3,16 @@ import "./ConferenceEvent.css";
 import TotalCost from "./TotalCost";
 import { useSelector, useDispatch } from "react-redux";
 import { incrementQuantity, decrementQuantity } from "./venueSlice";
+import {incrementAvQuantity, decrementAvQuantity} from "./avSlice";
+import { toggleMealSelection } from "./mealsSlice";
+
+
 const ConferenceEvent = () => {
     const [showItems, setShowItems] = useState(false);
     const [numberOfPeople, setNumberOfPeople] = useState(1);
     const venueItems = useSelector((state) => state.venue);
+    const avItems = useSelector((state) => state.av);
+    const mealsItems = useSelector((state) => state.meals)
     const dispatch = useDispatch();
     const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
 
@@ -29,17 +35,58 @@ const ConferenceEvent = () => {
         }
       };
     const handleIncrementAvQuantity = (index) => {
+      dispatch(incrementAvQuantity(index));
     };
 
     const handleDecrementAvQuantity = (index) => {
+      dispatch(decrementAvQuantity(index));
     };
 
     const handleMealSelection = (index) => {
-       
+      const item = mealsItems[index];
+      if(item.selected && item.type === 'mealForPeople')
+        {
+          //ensure number of people are set before toggling selection
+          const newNumberOfPeople = item.selected ? numberOfPeople: 0;
+          dispatch(toggleMealSelection(index, newNumberOfPeople));
+        }
+        else
+        {
+          dispatch(toggleMealSelection(index));
+        } 
     };
 
     const getItemsFromTotalCost = () => {
         const items = [];
+        venueItems.forEach((item) => {
+          if(item.quantity > 0)
+          {
+            items.push({...item, type: "venue"});
+          }
+        })
+
+        avItems.forEach((item) =>{
+          if (
+            item.quantity > 0 &&
+            !items.some((i) => i.name === item.name && i.type === "av")
+          )
+          {
+            items.push({...item, type: "av"});
+          }
+        })
+
+        mealsItems.forEach((item) =>{
+          if(item.selected)
+          {
+            const itemForDisplay = {...items, type: "meals"};
+            if(item.numberOfPeople)
+            {
+              itemForDisplay.numberOfPeople = numberOfPeople;
+            }
+            items.push(itemForDisplay);
+          }
+        });
+        return items;
     };
 
     const items = getItemsFromTotalCost();
@@ -54,9 +101,23 @@ const ConferenceEvent = () => {
             totalCost += item.cost * item.quantity;
           });
         }
+        else if (section === "av")
+        {
+          avItems.forEach((item) => {
+            totalCost += item.cost * item.quantity;
+          });
+        }
+        else if (section === 'meals')
+        {
+          mealsItems.forEach((item) => {
+            totalCost += item.cost * item.quantity;
+          })
+        }
         return totalCost;
       };
     const venueTotalCost = calculateTotalCost("venue");
+    const avTotalCost = calculateTotalCost("av");
+    const mealsTotalCost = calculateTotalCost("meals")
 
     const navigateToProducts = (idType) => {
         if (idType == '#venue' || idType == '#addons' || idType == '#meals') {
@@ -65,6 +126,11 @@ const ConferenceEvent = () => {
           }
         }
       }
+      const totalCosts = {
+        venue: venueTotalCost,
+        av: avTotalCost,
+        meals: mealsTotalCost,
+      };  
 
     return (
         <>
@@ -157,9 +223,22 @@ const ConferenceEvent = () => {
 
                                 </div>
                                 <div className="addons_selection">
-
+                                  {avItems.map((item, index) =>(
+                                    <div className="av_data venue_main" key={index}>
+                                      <div className="img">
+                                        <img src={item.img} alt={item.name} />
+                                      </div>
+                                      <div className="text"> {item.name} </div>
+                                      <div> ${item.cost} </div>
+                                      <div className="addons_btn">
+                                        <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
+                                        <span className="quantity-value"> {item.quantity} </span>
+                                        <button className="btn-success" onClick={() => handleIncrementAvQuantity(index)}> &#43 </button>
+                                      </div>
+                                    </div>
+                                  ))}
                                 </div>
-                                <div className="total_cost">Total Cost:</div>
+                                <div className="total_cost">Total Cost: {avTotalCost}</div>
 
                             </div>
 
@@ -173,12 +252,27 @@ const ConferenceEvent = () => {
                                 </div>
 
                                 <div className="input-container venue_selection">
-
+                                  <label htmlFor="numberOfPeople"><h3>Number of People</h3></label>
+                                  <input type="number" className="input_box5" id="numberOfPeople" value={numberOfPeople} 
+                                  onChange={(e) => setNumberOfPeople(parseInt(e.target.value))}
+                                  min="1"
+                                  />
                                 </div>
                                 <div className="meal_selection">
-
+                                  {mealsItems.map((item, index) => (
+                                    <div className="meal_item" key={index} style={{padding: 15}}>
+                                      <div className="inner">
+                                        <input type="checkbox" id={`meal_${index}`}
+                                        checked= {item.selected}
+                                        onChange={() => handleMealSelection(index)}
+                                        />
+                                        <label htmlFor={`meal_${index}`}> {item.name} </label>
+                                      </div>
+                                      <div className="meal_cost"> ${item.cost} </div>
+                                    </div>
+                                  ))}
                                 </div>
-                                <div className="total_cost">Total Cost: </div>
+                                <div className="total_cost">Total Cost: {mealsTotalCost} </div>
 
 
                             </div>
--- src/avSlice.js
+++ src/avSlice.js
@@ -3,16 +3,59 @@ import { createSlice } from "@reduxjs/toolkit";
 export const avSlice = createSlice({
   name: "av",
   initialState: [
+    {
+      img: "https://cdn.pixabay.com/photo/2014/08/26/21/59/projector-428664_1280.jpg",
+      name: "Projectors",
+      cost: 200,
+      quantity: 0,
+    },
+
+    {
+      img: "https://cdn.pixabay.com/photo/2016/11/29/04/40/accessories-1867368_1280.jpg",
+      name: "Speakers",
+      cost: 35,
+      quantity: 0,
+    },
+
+    {
+      img: "https://cdn.pixabay.com/photo/2017/08/06/15/44/microphone-2593567_1280.jpg",
+      name: "Microphones",
+      cost: 45,
+      quantity: 0,
+    },
+
+    {
+      img: "https://cdn.pixabay.com/photo/2023/12/04/14/58/table-8429665_1280.jpg",
+      name: "Whiteboards",
+      cost: 80,
+      quantity: 0,
+    },
+
+    {
+      img: "https://cdn.pixabay.com/photo/2017/08/02/07/55/directory-2570250_1280.jpg",
+      name: "Signage",
+      cost: 80,
+      quantity: 0,
+    },
     
   ],
 
 
   reducers: {
     incrementAvQuantity: (state, action) => {
+      const item = state[action.payload];
+      if(item)
+      {
+        item.quantity++;
+      }
       
     },
     decrementAvQuantity: (state, action) => {
-     
+      const item = state[action.payload];
+      if(item)
+      {
+        item.quantity--;
+      }
     },
   },
 });
